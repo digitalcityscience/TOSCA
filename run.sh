41cd51fea4a8918cda76e2486e5bfa9ab02be9db
Jan Behrens <jan.behrens@hcu-hamburg.de>
2020-04-15 14:06:32 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 /usr/share/geoserver/bin/startup.sh | while read line; do echo -e "\033[0;36m[GeoServer] $line\033[0m"; done &
-./scripts/base/ca_starter.sh | while read line; do echo -e "\033[0;32m[Cityapp GIS] $line\033[0m"; done &
+./scripts/base/cityapp_onesecond.sh | while read line; do echo -e "\033[0;32m[Cityapp GIS] $line\033[0m"; done &
+./scripts/base/cityapp_module_launcher.sh | while read line; do echo -e "\033[0;32m[Cityapp GIS] $line\033[0m"; done &
 cd webapp; node app.js | while read line; do echo -e "\033[0;33m[Cityapp webapp] $line\033[0m"; done
