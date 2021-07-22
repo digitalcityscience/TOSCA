45f136cfa0e0c98cdcec152e69921d818f621206
Jan Behrens <36447927+janbehrens@users.noreply.github.com>
2021-07-22 15:57:47 +0200

    remove unnecessary context wrapper

--- reactApp/src/components/controls/steps/steps-container.js
+++ reactApp/src/components/controls/steps/steps-container.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { StepsContextProvider } from './steps-context-provider';
+import { StepsContext } from './steps-context';
 
-export const StepsContainer = ({ id, className, steps, currentStep, children }) => {
-  const container = (
-    <div id={id} className={className}>
-      {children}
-    </div>
-  );
-  return (
-    <StepsContextProvider currentStep={currentStep} steps={steps}>
-      {container}
-    </StepsContextProvider>
-  );
-};
+export const StepsContainer = ({ steps, currentStep, children }) => (
+  <StepsContext.Provider value={{ currentStep, steps }}>
+    {children}
+  </StepsContext.Provider>
+);
 
 StepsContainer.propTypes = {
-  id: PropTypes.string,
-  className: PropTypes.string,
   steps: PropTypes.arrayOf(PropTypes.string).isRequired,
   currentStep: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
-};
-
-StepsContainer.defaultProps = {
-  id: null,
-  className: null,
 };
